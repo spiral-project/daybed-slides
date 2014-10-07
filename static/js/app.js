e5509c8a3bcb01b775c2f91c481d6764e3f925f1
Rémy HUBSCHER <rhubscher@mozilla.com>
2014-10-07 18:43:46 +0200

    Add the SlideList component.

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,40 @@
 /** @jsx React.DOM */
+var SlidesList = React.createClass({
+  getInitialState: function () {
+    return {
+      slides: [{title: "Markdown pour les nuls", id: 1},
+               {title: "ReST pour les nuls", id: 2}]
+    };
+  },
+
+  render: function() {
+    return (<div className="slidesList">
+      <table>
+        {this.state.slides.map(function(slide) {
+          return (
+            <tr>
+              <th>{slide.title}</th>
+              <td><button data-id={slide.id}>Edit</button><button data-id={slide.id}>Remove</button></td>
+             </tr>
+           );
+         })}
+      </table>
+      <button onClick={this.addSlideshow}>Add a new Slideshow</button>
+    </div>);
+  },
+
+  addSlideshow: function() {
+    this.setState({
+      slides: this.state.slides.concat([{title: "New", id: 3}])
+    });
+  }
+});
+
+
 React.renderComponent(
-  <h1>Hello, world!</h1>,
+  <div>
+    <h1>Your slides</h1>
+    <SlidesList />
+  </div>,
   document.getElementById('work')
 );
